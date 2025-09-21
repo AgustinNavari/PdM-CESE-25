cb8510e8caf763b50414276fdcebe1129aabeadc
Agustin Navari <navari.agustin@gmail.com>
2025-09-21 17:37:05 -0300

    Practica 4, Punto 1

---
+++ practica_4_punto_1/Drivers/API/Src/API_delay.c
@@ -0,0 +1,40 @@
+#include "API_delay.h"
+#include "stm32f4xx_hal.h"
+
+static bool_t delayIsRunning(delay_t * delay){
+	return delay->running;
+}
+
+void delayInit(delay_t * delay, tick_t duration){
+
+	//delay->startTime = 0;
+	delay->duration = duration;
+	delay->running = false;
+
+}
+
+bool_t delayRead(delay_t * delay){
+
+	if (delay->running == false){
+		delay->startTime = HAL_GetTick();
+		delay->running = true;
+	}
+
+	if ((delay->running == true) && (HAL_GetTick() - delay->startTime > delay->duration)){
+
+		delay->running = false;
+		return true; //si paso el tiempo devuelve "true"
+
+	}else{
+		return false; //si aun no paso el tiempo devuelve "false"
+	}
+}
+
+void delayWrite(delay_t * delay, tick_t duration){
+
+	if(!delayIsRunning(delay)){
+		delay->duration = duration;
+	}
+}
+
+
